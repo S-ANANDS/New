a09e6f8ee0c0e35708be5ec958491d6563de4189
Anand <anandnair96@gmail.com>
2019-07-07 02:58:44 +0530

    Everything Working

--- Main.rb
+++ Main.rb
@@ -5,7 +5,7 @@ db = SQLite3::Database.open('Photo.db')
 db.execute 'CREATE TABLE IF NOT EXISTS Tasks(Id INTEGER PRIMARY KEY,Name VARCHAR)'
 # db.execute " INSERT INTO Tasks(Id,Name) VALUES (NULL,'aa') "
 a=db.execute "SELECT * FROM Tasks"
-for i in 0..(a.length)
+for i in 0..(a.length-1)
   for j in 0..(photos.length)
       # binding.pry
     if a[i][1]==photos[j]
@@ -13,5 +13,6 @@ for i in 0..(a.length)
       break
     end
   end
+  # binding.pry
 end
 # binding.pry
